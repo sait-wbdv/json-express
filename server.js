3cfdf108b82cb80db6bf25a0ffdd23b25074051a
Tony Grimes <acidtone@tonygrimes.com>
2021-03-04 23:11:14 -0700

    added v0 to endpoint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/definitions', function(request, response){
+app.get('/api/v0/definitions', function(request, response){
   response.json(definitions);
 })
 
