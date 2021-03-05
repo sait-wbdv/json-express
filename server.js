d8d1bce917f5a689a725a552cf80591737268b33
Tony Grimes <acidtone@tonygrimes.com>
2021-03-04 22:25:47 -0700

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const definitions = require('./definitions');
 
 const app = express();
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/definitions', function(request, response){
